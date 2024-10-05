2bd6fae8d7ab5f632250a0f332e13ed3053df10e
fujiriku3988 <kd1350208@st.kobedenshi.ac.jp>
2024-10-06 02:47:49 +0900

    レッスンシェーダー完成、アウトラインもやった

---
+++ L10_OutLineShader/Src/Application/GameObject/Terrain/Terrain.cpp
@@ -0,0 +1,49 @@
+﻿#include "Terrain.h"
+
+void Terrain::Init()
+{
+	if (!m_spModel)
+	{
+		m_spModel = std::make_shared<KdModelWork>();
+		m_spModel->SetModelData("Asset/LessonData/Stage/StageMap1.gltf");
+
+		m_pCollider = std::make_unique<KdCollider>();
+		m_pCollider->RegisterCollisionShape("Ground", m_spModel, KdCollider::TypeGround);
+	}
+}
+
+void Terrain::Update()
+{
+	if (GetAsyncKeyState('1') & 0x8000)
+	{
+		if (!a)
+		{
+			m_spModel->SetModelData("Asset/LessonData/Stage/StageMap.gltf");
+			a = true;
+		}
+	}
+	else
+	{
+		a = false;
+	}
+	if (GetAsyncKeyState('2') & 0x8000)
+	{
+		if (!b)
+		{
+			m_spModel->SetModelData("Asset/LessonData/Stage/StageMap1.gltf");
+			b = true;
+		}
+	}
+	else
+	{
+		b = false;
+	}
+}
+
+void Terrain::DrawLesson()
+{
+	if (!m_spModel) return;
+
+	KdShaderManager::Instance().m_LessonShader.DrawModel(*m_spModel->GetData());
+}
+
---
+++ L10_OutLineShader/Src/Application/GameObject/Terrain/Terrain.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+class Terrain : public KdGameObject
+{
+public:
+	Terrain() {}
+	virtual ~Terrain()		override {}
+
+	void Init()				override;
+	void Update()			override;
+	void DrawLesson()		override;
+
+private:
+	std::shared_ptr<KdModelWork> m_spModel = nullptr;
+	bool a = false;
+	bool b = false;
+};
