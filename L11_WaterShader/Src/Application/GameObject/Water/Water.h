7eba307b504f65831af6fda1b1ba00503803104e
fujiriku3988 <kd1350208@st.kobedenshi.ac.jp>
2024-10-25 06:40:21 +0900

    水面シェーダーやった

---
+++ L11_WaterShader/Src/Application/GameObject/Water/Water.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+class Water : public KdGameObject
+{
+public:
+	Water() {}
+	virtual ~Water()		override {}
+
+	void Init()				override;
+	void Update()			override;
+	void DrawLit()		override;
+	void DrawLesson()		override;
+
+private:
+	std::shared_ptr<KdModelWork> m_spModel = nullptr;
+	bool a = false;
+	bool b = false;
+	Math::Vector2 m_UVOffSet;
+};
